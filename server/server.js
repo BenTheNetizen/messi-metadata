7c74f4fa26d5540a7dcfd3054769c3d2de741afe
Max Yuan <maxyuan6717@gmail.com>
2022-11-26 12:08:39 -0800

    connect to sqlite3 db

--- server/server.js
+++ server/server.js
@@ -2,12 +2,31 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = 4000;
+const sqlite3 = require("sqlite3").verbose();
+
+// open the database
+let db = new sqlite3.Database(
+  "../data/football.db",
+  sqlite3.OPEN_READONLY,
+  (err) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log("Connected to the football database.");
+  }
+);
 
 app.use(express.json());
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  // return first 10 rows
+  db.all("SELECT * FROM player_cleaned LIMIT 10", (err, rows) => {
+    if (err) {
+      throw err;
+    }
+    res.send(rows);
+  });
 });
 
 app.listen(port, () => {
