888d571150fd6491d03f81da38fabcb7fe12ec8e
Max Yuan <maxyuan6717@gmail.com>
2022-11-26 11:21:15 -0800

    init server

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = 4000;
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
